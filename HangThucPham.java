1e9c3357daa9ee9eef26b8c680353361a83c36ae
nguyenminhphu212 <25630641.phu@student.iuh.edu.vn>
2026-09-04 23:04:41 +0700

    Tuan 3

--- HangThucPham.java
+++ HangThucPham.java
@@ -1,13 +1,13 @@
 import java.time.LocalDate;
-import java.util.UUID;
-
+import java.time.format.DateTimeFormatter;
+import  java.util.UUID;
 public class HangThucPham {
     private String maHang;
     private String tenHang;
     private double donGia;
     private LocalDate ngaySanXuat;
     private LocalDate ngayHetHan;
-    public HangThucPham(String maHang, String tenHang, double donGia, LocalDate ngaySanXuat, LocalDate ngayHetHan) {
+    public HangThucPham(String tenHang, double donGia, LocalDate ngaySanXuat, LocalDate ngayHetHan) {
         this.maHang = UUID.randomUUID().toString();
         setTenHang(tenHang);
         setDonGia(donGia);
@@ -15,7 +15,7 @@ public class HangThucPham {
         setNgayHetHan(ngayHetHan);  
     }
     public void setTenHang(String tenHang) {
-        if(tenHang != null || !tenHang.isEmpty()) {
+        if(tenHang != null && !tenHang.isEmpty()) {
             this.tenHang = tenHang;
         }
         else {
@@ -31,7 +31,7 @@ public class HangThucPham {
         }
     }
     public void setNgaySanXuat(LocalDate ngaySanXuat) {
-        if(ngaySanXuat.isBefore(ngayHetHan)) {
+        if(ngaySanXuat.isBefore(LocalDate.now())) {
             this.ngaySanXuat = ngaySanXuat;
         }
         else {
@@ -45,5 +45,43 @@ public class HangThucPham {
         else {
             throw new IllegalArgumentException("Khong hop le");
         }
+    }
+    public String getMaHang() {
+        return maHang;
+    }
+    public String getTenHang() {
+        return tenHang;
+    }
+    public double getDonGia() {
+        return donGia;
+    }
+    public LocalDate getNgayHetHan() {
+        return ngayHetHan;
+    }
+    public LocalDate getNgaySanXuat() {
+        return ngaySanXuat;
+    }
+    public boolean  checkNgayHetHan() {
+        if(LocalDate.now().isAfter(ngayHetHan) || LocalDate.now().isEqual(ngayHetHan)) {
+            System.out.println("Da het han");
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return String.format("%-5s  %-30s  %,10.2f  %-15s  %-15s  %-5s", maHang, tenHang, donGia, ngaySanXuat.format(formatter), ngayHetHan.format(formatter), checkNgayHetHan());
+    }
+    public static void main(String[] args) {
+        HangThucPham hang1 = new HangThucPham("Gao", 100000, LocalDate.of(2026, 7, 10), LocalDate.of(2027, 7, 10));
+        HangThucPham hang2 = new HangThucPham("Mi", 5000, LocalDate.of(2026, 7, 10), LocalDate.of(2028, 10, 1)); 
+        HangThucPham hang3 = new HangThucPham("Nuoc", 10000, LocalDate.of(2026, 3, 01), LocalDate.of(2026, 9, 04));
+        System.out.println(String.format("%-40s  %-30s  %15s  %-15s  %-15s  %-15s", "Ma hang", "Ten hang", "Don gia", "Ngay san xuat", "Ngay het han", "Ghi chu"));
+        System.out.println(hang1);
+        System.out.println(hang2);
+        System.out.print(hang3); 
     }
 }
